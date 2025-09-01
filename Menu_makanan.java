4d849fe696739c3ffff77a38cf1a394651f9d953
Yobee_Code <150103646+OkslineItsFine@users.noreply.github.com>
2025-09-01 20:31:29 +0700

    Add files via upload

---
+++ Menu_makanan.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package menu_makanan;
+import java.util.Scanner;
+
+public class Menu_makanan {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+            Scanner input = new Scanner(System.in);
+        
+        System.out.println("=== Menu Paket Makanan ===");
+        System.out.println("1. Paket Ayam - Rp. 15.000");
+        System.out.println("2. Paket Ikan - Rp. 20.000");
+        System.out.println("3. Paket Daging - Rp. 30.000");
+        System.out.println("4. Paket Sayur - Rp. 10.000");
+        System.out.print("Pilih paket (1-4): ");
+        int pilihan = input.nextInt();
+        
+        int harga = 0;
+        String paket = "";
+        
+        switch (pilihan) {
+            case 1:
+                paket = "Paket Ayam";
+                harga = 15000;
+                break;
+            case 2:
+                paket = "Paket Ikan";
+                harga = 20000;
+                break;
+            case 3:
+                paket = "Paket Daging";
+                harga = 30000;
+                harga -= 5000; // potongan harga Rp.5000
+                break;
+            case 4:
+                paket = "Paket Sayur";
+                harga = 10000;
+                break;
+            default:
+                System.out.println("Pilihan tidak tersedia.");
+                return; // keluar dari program jika pilihan tidak valid
+    }
+            System.out.println("Anda memilih: " + paket);
+            System.out.println("Total Harga: Rp. " + harga);
+    }
+}
